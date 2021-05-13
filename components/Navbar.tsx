ae893140b15b1a53b1ea52a8fc9dd154b1e80c89
amienamry <amienamry@outlook.com>
2021-05-13 22:43:50 +0800

    Temp remove navbar options

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
 					<Image src='/images/logo/android-chrome-192x192.png' alt='amienamry' width={50} height={50} />
 				</a>
 			</Link>
-			<button
+			{/* <button
 				className='inline-flex p-3 hover:text-gray-200 rounded lg:hidden text-gray-100 ml-auto outline-none'
 				onClick={handleClick}>
 				<svg
@@ -28,9 +28,9 @@ export const Navbar = () => {
 					xmlns='http://www.w3.org/2000/svg'>
 					<path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M4 6h16M4 12h16M4 18h16' />
 				</svg>
-			</button>
+			</button> */}
 			{/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-			<div className={`${active ? '' : 'hidden'} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
+			{/* <div className={`${active ? '' : 'hidden'} w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
 				<div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start bg flex flex-col lg:h-auto'>
 					<Link href='/'>
 						<a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-100 text-xl items-center justify-center hover:text-gray-200'>
@@ -43,7 +43,7 @@ export const Navbar = () => {
 						</a>
 					</Link>
 				</div>
-			</div>
+			</div> */}
 		</nav>
 	);
 };
