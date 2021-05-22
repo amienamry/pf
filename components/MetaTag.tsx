1f732b5fda0ca7115917c0a28b51f4a8a9f9da6a
amienamry <amienamry@outlook.com>
2021-05-22 14:39:16 +0800

    Added meta title in metatag

--- components/MetaTag.tsx
+++ components/MetaTag.tsx
@@ -6,6 +6,7 @@ const MetaTag = (props: MetaDataType) => {
 		<Head>
 			<title> {props.title} </title>
 			<link rel='shortcut icon' href='/favicon.ico' />
+			<meta name='title' content={props.title} />
 			<meta name='description' content={props.description} />
 			<meta name='viewport' content='width=device-width, initial-scale=1' />
 
