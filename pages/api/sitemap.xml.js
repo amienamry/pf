d78ed2dddd71afb52d97c5d5f28543ae90a11f29
amienamry <amienamry@outlook.com>
2021-11-27 12:32:57 +0800

    Update sitemap

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,47 +1,41 @@
-import { SitemapStream, streamToPromise } from 'sitemap';
-import { format } from 'date-fns';
+import { SitemapStream, streamToPromise } from "sitemap";
+import { format } from "date-fns";
+import socialMedias from "../../mock/socialMediaList";
+import songs from "../../mock/songList";
 
 export default async (req, res) => {
-	const paths = [
-		'/',
-		'/experience',
-		'/education',
-	];
+	const paths = ["/", "/experience", "/education"];
+
+	socialMedias.forEach((sm) => {
+		paths.push(`/r/${sm.key}`);
+	});
+
+	songs.forEach((song) => {
+		paths.push(`/streams/${song.key}`);
+	});
 
 	try {
 		const smStream = new SitemapStream({
 			hostname: `https://${req.headers.host}`,
 		});
 
-		// List of posts
-		// const posts = [];
-
-		// // Create each URL row
-		// posts.forEach((post) => {
-		// 	smStream.write({
-		// 		url: `/post/${post.slug}`,
-		// 		changefreq: 'daily',
-		// 		priority: 0.9,
-		// 	});
-		// });
-
 		paths.forEach((url, i) => {
 			if (i === 0) {
 				smStream.write({
 					url,
-					changefreq: 'weekly',
+					changefreq: "weekly",
 					priority: 1,
-					lastmod: format(new Date(), 'yyyy-MM-dd'),
+					lastmod: format(new Date(), "yyyy-MM-dd"),
 				});
 			} else {
 				smStream.write({
 					url,
-					changefreq: 'weekly',
+					changefreq: "weekly",
 					priority: 0.9,
-					lastmod: format(new Date(), 'yyyy-MM-dd'),
+					lastmod: format(new Date(), "yyyy-MM-dd"),
 				});
 			}
-		})
+		});
 
 		// End sitemap stream
 		smStream.end();
@@ -51,7 +45,7 @@ export default async (req, res) => {
 
 		// Change headers
 		res.writeHead(200, {
-			'Content-Type': 'application/xml',
+			"Content-Type": "application/xml",
 		});
 
 		// Display output to user
