beacff33a0ef03820892df57c8606b0f453887fb
amienamry <amienamry@outlook.com>
2023-01-08 01:37:20 +0800

    Update FreePalestine hashtag color animation

--- components/FreePalestine.tsx
+++ components/FreePalestine.tsx
@@ -4,12 +4,13 @@ const FreePalestine = () => {
 			href='https://quran.com/17/4'
 			rel='noopener noreferrer'
 			style={{
-				top: '5.5%',
-				left: '50%',
-				transform: 'translate(-50%, -50%)',
+				top: "5.5%",
+				left: "50%",
+				transform: "translate(-50%, -50%)",
 			}}
-			className='fixed text-white z-20 animate-pulse'
-			target='_blank'>
+			className='fixed text-white z-20'
+			target='_blank'
+		>
 			<h1 className='text-gradient-custom text-xl sm:text-3xl md:text-3xl lg:md:text-3xl xl:text-3xl 2xl:text-3xl 3xl:text-3xl font-bold'>
 				#FreePalestine
 			</h1>
