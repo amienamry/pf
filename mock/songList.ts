6bac22634ca5dc759e11a9838fae372b500e27d1
amienamry <amienamry@outlook.com>
2023-04-28 16:44:16 +0800

    Added base stream page with 'rekt' data

--- mock/songList.ts
+++ mock/songList.ts
@@ -3,131 +3,221 @@ import { Song } from "../types/Song";
 const songs: Song[] = [
 	{
 		key: "ale-uto",
-		title: "ALE UTO by Arai Junior",
+		title: "ALE UTO",
+		fullTitle: "ALE UTO by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'ALE UTO'",
-		path: "https://distrokid.com/hyperfollow/araijunior/ale-uto",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--F885E4E5-326E-4085-B2568F24EDF8C954--1630733923527--aleuto1500x1500.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "shake-it",
-		title: "Shake It by Arai Junior",
+		title: "Shake It",
+		fullTitle: "Shake It by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Shake It'",
-		path: "https://distrokid.com/hyperfollow/araijunior/shake-it",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--EE95DD4F-7649-4D33-844E51A4B5A34807--1631782760560--shakeit.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "bong",
-		title: "Bong by Arai Junior",
+		title: "Bong",
+		fullTitle: "Bong by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Bong'",
-		path: "https://distrokid.com/hyperfollow/araijunior/bong",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--9C705480-C540-4AE3-A454C706F02F2937--1631782991040--bong.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "lengleng",
-		title: "Lengleng by Arai Junior",
+		title: "Lengleng",
+		fullTitle: "Lengleng by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Lengleng'",
-		path: "https://distrokid.com/hyperfollow/araijunior/lengleng",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--B751E79D-4A71-4E41-9CE5484DC5D845BF--1631783301170--lengleng.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "badut",
-		title: "Badut by Arai Junior",
+		title: "Badut",
+		fullTitle: "Badut by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Badut'",
-		path: "https://distrokid.com/hyperfollow/araijunior/badut",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--2B7D70F9-60E7-4057-ACCA9C802D01FE4A--1631783907122--badut.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "bedal",
-		title: "Bedal by Arai Junior",
+		title: "Bedal",
+		fullTitle: "Bedal by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Bedal'",
-		path: "https://distrokid.com/hyperfollow/araijunior/bedal",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--E7D006F3-18BC-4733-BC00D6098ED543E6--1631784084007--bedal.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "reborn",
-		title: "Reborn by Arai Junior",
+		title: "Reborn",
+		fullTitle: "Reborn by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Reborn'",
-		path: "https://distrokid.com/hyperfollow/araijunior/reborn",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--3D048E25-E23D-4FA2-83EEF9D81E91AAFE--1631784366769--reborn.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "smks4bk",
-		title: "SMKS4BK by Arai Junior",
+		title: "SMKS4BK",
+		fullTitle: "SMKS4BK by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'SMKS4BK'",
-		path: "https://distrokid.com/hyperfollow/araijunior/smks4bk",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--FEE6B4A3-54EB-45FF-8FA0B6552D4C45CD--1631784858690--smks4.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "ale-uto-hardstyle-remix",
-		title: "ALE UTO (Hardstyle Remix) by Arai Junior",
+		title: "ALE UTO (Hardstyle Remix)",
+		fullTitle: "ALE UTO (Hardstyle Remix) by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'ALE UTO (Hardstyle Remix)'",
-		path: "https://distrokid.com/hyperfollow/araijunior/ale-uto-hardstyle-remix",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--B591C249-6D40-40D2-8796BB42411CF7F5--1631786205122--aleutoremix.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "anarchy",
-		title: "Anarchy by Arai Junior",
+		title: "Anarchy",
+		fullTitle: "Anarchy by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Anarchy'",
-		path: "https://distrokid.com/hyperfollow/araijunior/anarchy",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--730A5AC3-AB67-409D-A23E44326BF789CE--1631794010101--anarchy.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "boncai",
-		title: "Boncai by Arai Junior",
+		title: "Boncai",
+		fullTitle: "Boncai by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Boncai'",
-		path: "https://distrokid.com/hyperfollow/araijunior/boncai",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--FAB9F89D-B65D-4279-A4C6C87070F3D1A8--1631794432678--boncai.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "cincai",
-		title: "Cincai by Arai Junior",
+		title: "Cincai",
+		fullTitle: "Cincai by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Cincai'",
-		path: "https://distrokid.com/hyperfollow/araijunior/cincai",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--14FD437D-3AFD-42C7-AE5F357495140921--1631794612641--cincai.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "hush",
-		title: "Hush by Arai Junior",
+		title: "Hush",
+		fullTitle: "Hush by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Hush'",
-		path: "https://distrokid.com/hyperfollow/araijunior/hush",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--46FAA267-1DEB-4A99-83D3D301F32C6737--1631794798222--hush.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "null",
-		title: "null by Arai Junior",
+		title: "null",
+		fullTitle: "null by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'null'",
-		path: "https://distrokid.com/hyperfollow/araijunior/null",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--923FAA2C-5AD7-4B12-89C95716FCA2F626--1631795284525--null.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "rararera",
-		title: "Rararera by Arai Junior",
+		title: "Rararera",
+		fullTitle: "Rararera by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Rararera'",
-		path: "https://distrokid.com/hyperfollow/araijunior/rararera",
-		img_url:
+		imgThumb:
 			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--5BDCD56B-0E61-421D-9C0471E3E2D69113--1631795935758--rararera.jpg",
+		imgBg: "",
+		audioUrl: "",
+		platforms: [],
 	},
 	{
 		key: "rekt",
-		title: "Rekt by Arai Junior",
+		title: "Rekt",
+		fullTitle: "Rekt by Arai Junior",
+		artist: "Arai Junior",
 		description: "Stream and Save 'Rekt'",
-		path: "https://distrokid.com/hyperfollow/araijunior/rekt",
-		img_url:
-			"https://s3.amazonaws.com/gather.fandalism.com/300x300-3293743--3FEA014D-F2C3-44CC-B1986CE236063D22--1631796161431--rekt.jpg",
+		imgThumb: "/images/streams/rekt/thumb.jpeg",
+		imgBg: "/images/streams/rekt/bg.jpeg",
+		audioUrl:
+			"https://s3.amazonaws.com/audio.distrokid.com/preview_25795353_3DDCE651-0CB9-4DA4-86F67B1C58EFEC69.mp3",
+		platforms: [
+			{
+				name: "Spotify",
+				url: "https://open.spotify.com/album/0voWjNT5kDWCrwYTPGz1zS",
+				imgUrl: "/images/streams/spotify.jpeg",
+			},
+			{
+				name: "Apple Music",
+				url: "https://music.apple.com/us/album/rekt-single/1586199767",
+				imgUrl: "/images/streams/apple-music.jpeg",
+			},
+			{
+				name: "iTunes",
+				url: "https://music.apple.com/us/album/rekt-single/1586199767",
+				imgUrl: "/images/streams/itunes.jpeg",
+			},
+			{
+				name: "Amazon Music",
+				url: "https://www.amazon.com/gp/product/B09GH8V5D7",
+				imgUrl: "/images/streams/amazon-music.jpeg",
+			},
+			{
+				name: "Deezer",
+				url: "https://www.deezer.com/en/album/259367092",
+				imgUrl: "/images/streams/deezer.jpeg",
+			},
+		],
 	},
 ];
 
--- pages/streams/[song].tsx
+++ pages/streams/[song].tsx
@@ -1,8 +1,11 @@
 import { MetaDataType } from "../../types/MetaData";
 import MainLayout from "../../components/MainLayout";
 import { useRouter } from "next/router";
-import redirect from "nextjs-redirect";
 import songs from "../../mock/songList";
+import { Song, StreamingPlatform } from "../../types/Song";
+import Image from "next/image";
+import Link from "next/link";
+import { FaChevronRight } from "react-icons/fa";
 
 const Song = () => {
 	const router = useRouter();
@@ -10,50 +13,86 @@ const Song = () => {
 	const song = songs.find((sm) => sm.key === router.query.song);
 
 	if (!song) {
-		const Redirect = redirect("https://amienamry.dev");
-		const message = ["x"].reduce((acc, msg) => {
-			for (let i = 0; i < 10; i++) {
-				for (let j = 0; j < 200; j++) {
-					acc += msg;
-				}
-				acc += "\n";
-			}
-
-			return acc;
-		}, "x");
-
-		return (
-			<Redirect>
-				<Content message={message} />
-			</Redirect>
-		);
+		return "song not found";
 	}
 
 	const metaData: MetaDataType = {
-		title: song?.title,
-		description: song?.description,
-		image_url: song?.img_url,
-		path: `https://amienamry.dev/streams/${song?.key}`,
+		title: song.fullTitle,
+		description: song.description,
+		image_url: song.imgThumb,
+		path: `https://amienamry.dev/streams/${song.key}`,
 	};
 
-	const Redirect = redirect(song?.path);
-
 	return (
-		<Redirect>
-			<MainLayout
-				metaData={metaData}
-				Content={() => <Content message={song?.title} />}
-			/>
-		</Redirect>
+		<MainLayout
+			metaData={metaData}
+			Content={() => <Content song={song} />}
+		/>
 	);
 };
 
-const Content = ({ message }) => {
-	return <h1 className='text-black text-2xl'>{message}</h1>;
+const Content = ({ song }: { song: Song }) => {
+	return (
+		<>
+			<div className="fixed top-[-50%] left-[-50%] w-[200%] h-[200%] bg-black z-[-100] overflow-hidden">
+				<img
+					className="absolute top-0 left-0 right-0 bottom-0 m-auto min-w-[55%] min-h-[55%] blur-lg opacity-30"
+					src={song.imgBg}
+				/>
+			</div>
+			<div className="flex flex-col w-full max-w-2xl pt-20 sm:pt-36 text-gray-100 px-4 sm:px-0">
+				<div className="w-full flex flex-col items-center sm:bg-neutral-800 rounded-xl mb-5 bg-opacity-75">
+					<div className="relative w-48 h-48 -mt-8 mb-6">
+						<Image
+							className="absolute rounded-xl"
+							alt={`${song.title}'s album cover`}
+							src={song.imgThumb}
+							fill={true}
+						/>
+					</div>
+
+					<h1 className="text-3xl font-bold mb-2">{song.title}</h1>
+					<h3 className="text-lg mb-3 sm:mb-8">{song.artist}</h3>
+				</div>
+
+				<div className="w-full flex flex-col items-center bg-neutral-700 rounded-xl mb-5">
+					wave
+				</div>
+
+				{song.platforms.map((platform) => (
+					<Platform platform={platform} />
+				))}
+			</div>
+		</>
+	);
 };
 
-Song.getInitialProps = async () => {
-	return {};
+const Platform = ({ platform }: { platform: StreamingPlatform }) => {
+	return (
+		<div className="w-full flex flex-row items-center bg-neutral-700 rounded-xl mb-5 hover:bg-neutral-700 bg-opacity-75">
+			<Link
+				className="w-full flex flex-row"
+				href={platform.url}
+				target="_blank"
+			>
+				<Image
+					className="rounded-l-xl"
+					alt={`${platform.name}'s logo`}
+					src={platform.imgUrl}
+					width={65}
+					height={65}
+					style={{
+						boxShadow: "0px 0px 5px #000000",
+					}}
+				/>
+				<div className="flex flex-row w-full items-center justify-between px-4">
+					<p className="font-semibold">{platform.name}</p>
+
+					<FaChevronRight />
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default Song;
