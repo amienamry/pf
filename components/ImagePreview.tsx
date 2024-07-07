6466b993985f533892a9c36de02a99f37feba65b
amienamry <amienamry@outlook.com>
2024-07-07 17:43:35 +0800

    add songs

--- components/ImagePreview.tsx
+++ components/ImagePreview.tsx
@@ -28,9 +28,9 @@ export const ImagePreview = ({
 
 	return (
 		<>
-			<div className="flex flex-col w-full h-auto mt-20 items-center">
+			<div className='flex flex-col w-full h-auto mt-20 items-center'>
 				{!!path && (
-					<div className="flex min-h-screen w-full max-w-3xl flex-col bg-black bg-opacity-80 rounded-md">
+					<div className='flex min-h-screen w-full max-w-3xl flex-col bg-black bg-opacity-80 rounded-md'>
 						<div
 							style={{
 								maxHeight: window.screen.width,
@@ -40,13 +40,13 @@ export const ImagePreview = ({
 						>
 							{isLoading && (
 								<img
-									className="absolute left-0 right-0 top-0 bottom-0 m-auto w-16 h-16 z-50"
-									src="/images/loader.svg"
+									className='absolute left-0 right-0 top-0 bottom-0 m-auto w-16 h-16 z-50'
+									src='/images/loader.svg'
 								/>
 							)}
 							<Image
 								onLoad={() => loadComplete?.()}
-								className="flex  object-contain sm:rounded-lg w-auto h-auto"
+								className='flex object-contain w-auto h-auto'
 								width={window.screen.width}
 								height={1000}
 								src={path}
@@ -58,23 +58,23 @@ export const ImagePreview = ({
 							{image.location && image.locationUrl && (
 								<a
 									href={image.locationUrl}
-									target="_blank"
-									rel="noreferrer"
-									className="absolute text-sm flex flex-row items-center bg-black bg-opacity-70 m-1.5 pl-1 pr-2 py-1 rounded right-0"
+									target='_blank'
+									rel='noreferrer'
+									className='absolute text-sm flex flex-row items-center bg-black bg-opacity-70 m-1.5 pl-1 pr-2 py-1 rounded right-0'
 								>
-									<IoLocationSharp className="mr-1" />
+									<IoLocationSharp className='mr-1' />
 									{image.location}
 								</a>
 							)}
 						</div>
 
-						<div className="mt-4 px-4 flex flex-row ">
-							<div className="flex flex-col flex-1">
+						<div className='mt-4 px-4 flex flex-row '>
+							<div className='flex flex-col flex-1'>
 								{image.description && (
-									<p className="mb-2">{image.description}</p>
+									<p className='mb-2'>{image.description}</p>
 								)}
 								{image.createdAt && (
-									<p className="text-xs opacity-60">
+									<p className='text-xs opacity-60'>
 										{format(
 											image.createdAt,
 											"eeee, d MMM yyyy"
