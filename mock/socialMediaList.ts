dfe55e0afc30a75bbfb8540e7e9e776c548da399
amienamry <amienamry@outlook.com>
2025-06-22 15:55:20 +0800

    enable instagram acc redirection

--- mock/socialMediaList.ts
+++ mock/socialMediaList.ts
@@ -1,35 +1,40 @@
-import { SocialMedia } from "../types/SocialMedia";
+import { SocialMedia } from '../types/SocialMedia';
 
 const socialMedias: SocialMedia[] = [
 	{
-		key: "twitter",
-		title: "Twitter",
-		path: "https://twitter.com/amienamry",
+		key: 'twitter',
+		title: 'Twitter',
+		path: 'https://twitter.com/amienamry',
 	},
 	{
-		key: "spotify",
-		title: "Spotify",
-		path: "https://open.spotify.com/artist/3SwgFLDekh43vfME5GUVPd",
+		key: 'spotify',
+		title: 'Spotify',
+		path: 'https://open.spotify.com/artist/3SwgFLDekh43vfME5GUVPd',
 	},
 	{
-		key: "youtube",
-		title: "Youtube",
-		path: "https://youtube.com/araijunior",
+		key: 'youtube',
+		title: 'Youtube',
+		path: 'https://youtube.com/araijunior',
 	},
 	{
-		key: "soundcloud",
-		title: "SoundCloud",
-		path: "https://soundcloud.com/araijunior",
+		key: 'soundcloud',
+		title: 'SoundCloud',
+		path: 'https://soundcloud.com/araijunior',
 	},
 	{
-		key: "linkedin",
-		title: "LinkedIn",
-		path: "https://linkedin.com/in/amienamry",
+		key: 'linkedin',
+		title: 'LinkedIn',
+		path: 'https://linkedin.com/in/amienamry',
 	},
 	{
-		key: "github",
-		title: "Github",
-		path: "https://github.com/amienamry",
+		key: 'github',
+		title: 'Github',
+		path: 'https://github.com/amienamry',
+	},
+	{
+		key: 'instagram',
+		title: 'Instagram',
+		path: 'https://instagram.com/amienamry',
 	},
 ];
 
